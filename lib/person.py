5d4d5692a6e137a9376d5f33ce48c1dcb174e831
kimthedrew <kimutai848@gmail.com>
2024-12-17 10:11:23 +0300

    instance

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
+
+anyone = Person()
+anyone.talk()
+anyone.walk()
